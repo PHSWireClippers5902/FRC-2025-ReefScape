cee75e9ead4717c8e9840058950d344b7c00b622
Rage65 <123thetechguy@gmail.com>
2025-03-10 12:45:47 -0400

    Fixes to LedSubsystem.java
    Accounted for having 2 led strips on separate REV Blinkin's. Also fixed it addressing the wrong pwm port.

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -9,23 +9,29 @@ import frc.robot.commands.LedCommand;
 public class LedSubsystem extends SubsystemBase {
       // see https://rag3r.frii.site/files/blinkin.pdf to find the pwm values
     
-     Spark strip1 = new Spark(0);// servo is used to controll the led strip via pwm as there is no rev blinkin object
+     Spark strip1 = new Spark(9);// servo is used to controll the led strip via pwm as there is no rev blinkin object
+     Spark strip2 = new Spark(8);
     
    
         public void setGreen(){
             strip1.set(0.77);
+            strip2.set(-0.89);
         }
         public void setRed(){
             strip1.set(0.61);
+            strip2.set(-0.89);
         }
         public void setBlue(){
             strip1.set(0.87);
+            strip2.set(-0.89);
         }
         public void setParty(){
             strip1.set(-0.87);
+            strip2.set(-0.87);
         }
         public void setIdle(){
             strip1.set(-0.31);
+            strip2.set(-0.31);
         }
         public void setManual(double pwm){
             strip1.set(pwm);
