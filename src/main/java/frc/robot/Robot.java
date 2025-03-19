774af6440b2aee759f775844bae33a87e41edb69
Rage65 <123thetechguy@gmail.com>
2025-03-18 22:00:02 -0400

    Enabled USB 0 camera for cage alignment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -61,8 +61,8 @@ public class Robot extends TimedRobot {
     //SmartDashboard.putData("Auto choices", m_chooser);
     System.out.println("Robot Init - NOW");
     
-  //  CameraServer.startAutomaticCapture(0);
-  //  CameraServer.startAutomaticCapture(1);
+    CameraServer.startAutomaticCapture(0);
+    //CameraServer.startAutomaticCapture(1);
 
     // m_robotContainer.m_gyro.calibrate();
     //System.out.println("Calibrate!!");
